cb13085111a1f5f0050ea9f5019f7b45beefa58f
Wellington <juniozguedes@gmail.com>
2022-10-31 23:09:34 -0300

    Removed trash code from index

--- src/index.js
+++ src/index.js
@@ -3,17 +3,6 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  // Create a Map
-  const db = new Map();
-
-  // Set Map Values
-  db.set("apples", { color: "red", good: "yes" });
-  db.set("bananas", 300);
-  db.set("oranges", 200);
-
-  console.log(db);
-  const obj = db.get("apples");
-  console.log(obj);
   res.send("Hello World!");
 });
 
